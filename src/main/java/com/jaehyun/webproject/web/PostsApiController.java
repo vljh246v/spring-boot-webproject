4c22dd1c23299bd121f43494727a0bf2b49062b2
jaehyun <vljh246v@naver.com>
2020-10-27 23:47:10 +0900

    PostsApiController 생성 & save("/api/v1/posts") 메소드 구현

---
+++ src/main/java/com/jaehyun/webproject/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.jaehyun.webproject.web;
+
+import com.jaehyun.webproject.service.posts.PostsService;
+import com.jaehyun.webproject.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
