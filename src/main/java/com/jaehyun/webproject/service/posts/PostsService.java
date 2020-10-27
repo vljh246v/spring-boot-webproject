c253c7ae22f6c82a76a4e1697210fe27e5c36f3f
jaehyun <vljh246v@naver.com>
2020-10-28 00:37:39 +0900

    update / get 구현

--- src/main/java/com/jaehyun/webproject/service/posts/PostsService.java
+++ src/main/java/com/jaehyun/webproject/service/posts/PostsService.java
@@ -1,9 +1,13 @@
 package com.jaehyun.webproject.service.posts;
 
+import com.jaehyun.webproject.domain.posts.Posts;
 import com.jaehyun.webproject.domain.posts.PostsRepository;
+import com.jaehyun.webproject.web.dto.PostsResponseDto;
 import com.jaehyun.webproject.web.dto.PostsSaveRequestDto;
+import com.jaehyun.webproject.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +18,22 @@ public class PostsService {
 
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(posts);
     }
 }
