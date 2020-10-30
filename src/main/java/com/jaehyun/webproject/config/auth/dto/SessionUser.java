f936ec4a3816445bd3292823c6dda50d51eb408b
jaehyun <vljh246v@naver.com>
2020-10-30 22:13:24 +0900

    naver 로그인 추가

--- src/main/java/com/jaehyun/webproject/config/auth/dto/SessionUser.java
+++ src/main/java/com/jaehyun/webproject/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.jaehyun.webproject.config.auth.dto;
 import com.jaehyun.webproject.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
   private String name;
   private String email;
