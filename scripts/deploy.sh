c0e22deaabe33cf55bcda556406dcdfd4a4a1b10
jaehyun <vljh246v@naver.com>
2020-11-01 02:51:30 +0900

    배포 스크립트 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp /home/ec2-user/app/step2/zip/*.jar /home/ec2-user/app/step2/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl spring-boot-webproject | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl spring-boot-webproject | grep java | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
